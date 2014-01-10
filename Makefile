7fbd929cd6715c830873eb3e1ed326ea7bbb8b26
A.J. Bonnema <gbonnema@xs4all.nl>
2014-01-10 15:02:23 +0100

    * added GPL version 3 license * modified code to have copy right line plus license and warranty statements * added usage clause to devpkg * added note 9 to notes.txt * fixed DESTDIR in Makefile

--- Makefile
+++ Makefile
@@ -9,12 +9,11 @@ all: devpkg
 devpkg: bstrlib.o db.o shell.o commands.o
 
 install: all
-	install -d ${DESTDIR}/${PREFIX}/bin/
-	install devpkg ${DESTDIR}/${PREFIX}/bin
+	install -d $(DESTDIR)$(PREFIX)/bin/
+	install devpkg $(DESTDIR)$(PREFIX)/bin/
 
 clean:
 	rm -f *.o
 	rm -f devpkg
 	rm -rf *.dSYM
-
 
--- devpkg.c
+++ devpkg.c
@@ -1,3 +1,24 @@
+/*
+This program assists in adding lines to a file.
+
+Copyright (C) 2014  Zed A. Shaw		
+			  2014  Guus Bonnema	Copied program in Jan 2014 from Zed Shaw
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include <stdio.h>
 #include <apr_general.h>
 #include <apr_getopt.h>
@@ -94,7 +115,19 @@ int main(int argc, const char const *argv[])
 			break;
 
 		default:
-			sentinel("Invalid command given.");
+
+			sentinel("\nUSAGE: devpkg <command> [<options>]"
+					 "\n\tCommands:"
+					 "\n\t-I <url> \tInstall package from <url>"
+					 "\n\t-L\t\tList installed packages"
+					 "\n\t-S\t\tInitialize package database"
+					 "\n\t-F <url>\tFetch package from <url>"
+					 "\n\t-B <url>\tBuild package from <url>"
+					 "\n\n\tinstall options:"
+					 "\n\t-c <config-opts>"
+					 "\n\t-m <make-opts>"
+					 "\n\t-i <install-opts>"
+					);
 	}
 
 
